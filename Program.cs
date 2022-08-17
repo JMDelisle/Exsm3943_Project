2d8e84dfeb49c5d15a006556f6be3272ab1a0178
Grant Stensrud <stensrud@ualberta.ca>
2022-08-16 19:57:17 -0600

    Committing to practice the merging; Admin menu build started;

--- Program.cs
+++ Program.cs
@@ -7,8 +7,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Need to add an admin password and log in
-
         // Main Menu Loop
         bool successfull = false;
         string username = "";
@@ -17,13 +15,14 @@ class Program
     Start:
         while (!successfull)
         {
+            Console.Clear();
             // Main Menu Displayed
             Console.WriteLine("\n Bits and Bytes Autobody Shop\t\tNOT LOGGED IN\n\t\t\tMain Menu\n\t\t\n\n1) Log In\n\t\t2) Admin Menu\n\t\t3) Exit\n");
             Console.Write(" Please choose: ");
             var input = Console.ReadLine();
             if (input == "1")
             {
-                Console.Write("\n Enter your account Phone Number ");
+                Console.Write("\n\tEnter your account Phone Number ");
                 var userNumber = Console.ReadLine();
                 // querry on database for the customers table on the phone number column
                 using (DatabaseContext context = new DatabaseContext())
@@ -35,7 +34,7 @@ class Program
                         // Test if the current customer.PhoneNumber = the phone number the user entered (in variable phonenumber)
                         if (userNumber != customer.PhoneNumber)
                         {
-                            Console.WriteLine("Phone Number not found, Would you Like to add it to the system?");
+                            Console.WriteLine("\n\tPhone Number not found, Would you Like to add it to the system?");
                             var answer = Console.ReadLine();
 
                             if (answer != "Y")
@@ -43,64 +42,123 @@ class Program
                                 goto Start;
                             }
 
-
-                        else
-                        {
-                            Console.WriteLine("Welcome Back " + customer.NameFirst +"!\n What Product Category are you lookiing for today?");
-                            foreach (ProductCategory productCategory in context.ProductCategories.ToList())
+                            else
                             {
-                                Console.WriteLine(productCategory.Id + " " + productCategory.CategoryName);
+                                Console.Clear();
+                                Console.WriteLine("\n\n\tWelcome Back " + customer.NameFirst + "!\n What Product Category are you lookiing for today?");
+                                foreach (ProductCategory productCategory in context.ProductCategories.ToList())
+                                {
+                                    Console.WriteLine(productCategory.Id + " " + productCategory.CategoryName);
+
+                                    // if category 1
+                                    // {
+                                    // display all products from category 1
+                                    Console.WriteLine("What product would you like to buy?");
+                                    var purcahseItem = Console.ReadLine();
+                                    // }
+
+                                    // if category 2
+                                    // {
+                                    // display all products from category 2
+                                    // }
+
+                                    // if category 3
+                                    // {
+                                    // display all products from category 3
+                                    // }
+
+                                    // if category 4
+                                    // {
+                                    // display all products from category 4
+                                    // }
+
+                                    // if category 5
+                                    // {
+                                    // display all products from category 5
+                                    // }
+
+                                    // if category 6
+                                    // {
+                                    // display all products from category 6
+                                    // }
+
+
+                                }
                             }
                         }
-                        }
-                        // test flag if true {found customer do stuff}
-                        // else{no matching customer found enter new cuystomer or new phone number}
-                    }
-                    // Login Bits and Bytes Admin
-                    if (input == "2")
-                    {
-                    AdminName:
-                        Console.Write("\n Enter the Admin User Name: ");
-                        username = Console.ReadLine();
-                        if (username != adminName)
+                        // Login Bits and Bytes Admin
+                        if (input == "2")
                         {
-                            Console.WriteLine("Incorrect.  Try again");
-                            goto AdminName;
-                        }
-                        else
-                        {
-                        Password:
-                            Console.Write(" Enter the Admin password: ");
-                            var userPassword = Console.ReadLine();
-                            if (userPassword != adminPassword)
+                        AdminName:
+                            Console.Clear();
+                            Console.Write("\n Enter the Admin User Name: ");
+                            username = Console.ReadLine();
+                            if (username != adminName)
                             {
                                 Console.WriteLine("Incorrect.  Try again");
-                                goto Password;
+                                goto AdminName;
                             }
                             else
-                            {//all admin menu code below this 
-                             //here we need to build and code the admin menu
+                            {
+                            Password:
+                                Console.Clear();
+                                Console.Write("\n\nEnter the Admin password: ");
+                                var userPassword = Console.ReadLine();
+                                if (userPassword != adminPassword)
+                                {
+                                    Console.WriteLine("Incorrect.  Try again");
+                                    goto Password;
+                                }
+                                else
+                                {//all admin menu code below this 
+                                 //here we need to build and code the admin menu
 
-                                /*menu needs to have 
-                                1)Add Stock
-                                2)Remove Stock        
-                                5)Flag product as discontinued
-                                0)Exit
-                                */
+                                    /*menu needs to have 
+                                    1)Add Stock
+                                    2)Remove Stock        
+                                    3)Flag product as discontinued
+                                    0)Exit
+                                    */
+                                    int adminSelection = -1;
+                                    while (adminSelection != 3)
+                                    {
 
 
-                            }//all admin menu code above this
-                        }
-                        // Exit Bits and Byyes Sales System
-                        if (input == "3")
-                        {
-                            Console.WriteLine("\n Goodbye " + username + "\n");
-                            successfull = true;
-                        }
-                        // Invalid entry by user :(
-                        else
-                        {
-                            Console.WriteLine("\n * * * Invalid Menu Selection, Try again * * *\n");
+                                        Console.Clear();
+                                        Console.WriteLine("\n\n\t\t\tBits & Bytes Admin Menu\n\n\t\t1) Add Stock to Inventory\n\t\t2) Remove Stock from Inventory\n\t\t3) Flag Stock as DISCONTINUED\n\t\t0) Log Out of ADMIN MENU\n\n\t\t\tPlease make your selection: ");
+                                        try
+                                        {
+                                            adminSelection = Int32.Parse((Console.ReadLine() ?? "").Trim());
+
+
+
+
+
+
+
+
+                                        }
+                                        catch
+                                        {
+                                            break;
+                                        }
+
+                                    }
+                                }//all admin menu code above this
+                            }
+                            // Exit Bits and Byyes Sales System
+                            if (input == "3")
+                            {
+                                Console.WriteLine("\n\n\tGoodbye " + username + "\n");
+                                successfull = true;
+                            }
+                            // Invalid entry by user :(
+                            else
+                            {
+                                Console.WriteLine("\n * * * Invalid Menu Selection, Try again * * *\n\tPress any key to continue");
+                                Console.ReadKey();
+                                goto Start;
+                            }
                         }
                     }
                 }
